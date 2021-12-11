1e55c7c723b8e53d1d0825c1cdcac559cec55603
Agamdeep Singh <agamdeep579@gmail.com>
2021-12-11 16:54:45 +0530

    Added the code

---
+++ main.py
@@ -0,0 +1,26 @@
+# The below function calculates if a year is leap year or not
+# Created by Agamdeep Singh / CodeWithAgam
+# Youtube: CodeWithAgam
+# Github: CodeWithAgam
+# Instagram: @agamdeep_21, @coderagam001
+# Twitter: @CoderAgam001
+# Linkdin: Agamdeep Singh
+
+# Print a welcome message
+
+# Get the year from the user
+year = int(input("Which year do you want to check? "))
+
+# Check for the conditions
+# The Modulo Sign (%) calculates remainder after division of 2 numbers.
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+
+else:
+    print("Not leap year.") 
